567bc5fda9502707030029b4b4cb47a0b60f125a
rogereichert <rogereichert@gmail.com>
2022-12-10 06:05:37 -0300

    Tela relatorio_cliente.php datatable preenchida com dados do bd

--- paginas/conteudo/relatorio_cliente.php
+++ paginas/conteudo/relatorio_cliente.php
@@ -14,7 +14,8 @@
                   <!-- /.card-header -->
                   <table id="example1" class="table table-bordered table-striped">
                     <thead>
-                      <tr>
+                      <tr style="text-align: center;">
+                        <th>Código</th>
                         <th>Nome do Cliente</th>
                         <th>Whats</th>
                         <th>Endereço</th>
@@ -23,29 +24,46 @@
                       </tr>
                     </thead>
                     <tbody>
-                      <tr>
-                        <td>Roger Reichert</td>
-                        <td>51 981861766</td>
-                        <td>Rua X 123</td>
-                        <td>Caloteiro</td>
-                        <td>
-                          <button class="btn btn-success btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Editar"><i class="fa fa-edit"></i></button>
-                          <button class="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Deletar"><i class="fa fa-trash"></i></button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Sophia Mariana Affonso Furlan dos Santos</td>
-                        <td>11 11111111</td>
-                        <td>Rua AAAA 123</td>
-                        <td>Lala</td>
-                        <td>
-                          <button class="btn btn-success btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i></button>
-                          <button class="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"></i></button>
-                        </td>
-                      </tr>
+
+                      <?php
+                      require "../config/conexao.php";
+                      $select = "SELECT * FROM tbl_clientes ORDER BY id DESC";
+                      try {
+                        $result = $conect->prepare($select);
+                        $cont = 1;
+                        $result->execute();
+
+                        $contar = $result->rowCount();
+                        if ($contar > 0) {
+                          while ($show = $result->FETCH(PDO::FETCH_OBJ)) {
+
+                      ?>
+                            <tr>
+                              <td><?php echo $cont++; ?></td>
+                              <td><?php echo $show->nome ?></td>
+                              <td><?php echo $show->whats ?></td>
+                              <td><?php echo $show->endereco ?></td>
+                              <td><?php echo $show->observacoes ?></td>
+                              <td>
+                                <button class="btn btn-success btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Editar"><i class="fa fa-edit"></i></button>
+                                <button class="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Deletar"><i class="fa fa-trash"></i></button>
+                              </td>
+                            </tr>
+
+                      <?php
+
+                          } // fim do while
+                        } // fim do if
+                      } catch (PDOException $e) {
+                        echo '<strong>ERRO DE PDO= </strong>' . $e->getMessage();
+                      }
+
+                      ?>
+
                     </tbody>
                     <tfoot>
                       <tr>
+                      <th style="text-align: center;">Código</th>
                         <th>Nome do Cliente</th>
                         <th>Whats</th>
                         <th>Endereço</th>
